71b2f48dfb05927fdc237b0c0852536be0bb3d69
Lixin-SCUT <im.lixin@foxmail.com>
2020-02-22 23:54:55 +0800

    2020.02.22

---
+++ examples/simple/timeclient/timeclient.cc
@@ -0,0 +1,89 @@
+// timeclient.cc
+// Created by Lixin on 2020.02.22
+
+#include "muduo/base/Logging.h"
+#include "muduo/net/Endian.h"
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/InetAddress.h"
+#include "muduo/net/TcpClient.h"
+
+#include <utility>
+
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+class TimeClient : noncopeable
+{
+public:
+	TimeClient(EventLoop *loop,const InetAddress &serverAddr)
+		: loop_(loop),
+		  client_(loop,serverAddr,"TimeClient")
+	{
+		client_.setConnectionCallback(
+				std::bind(&onConnection,this,_1));
+		client_setMessageCallback(
+				std::bind(&onMessage,this,_1,_2,_3));
+	}
+
+	void connect()
+	{
+		client_.connect();
+	}
+private:
+	EventLoop* loop_;
+	TcpClient client_;
+
+	void onConnection(const TcpConnectionPtr &conn)
+	{
+		LOG_INFO << conn->localAddress().toIpPort() << " -> "
+             		<< conn->peerAddress().toIpPort() << " is "
+             		<< (conn->connected() ? "UP" : "DOWN");
+
+  	 	if (!conn->connected())
+    		{
+      			loop_->quit();
+    		}
+	}
+
+	void onMessage(const TcpConneciontPtr &conn,Buffer *buf,Timestamp receiveTime)
+	{
+		if(buf->readableBytes() >= sizeof(int32_t))
+		{
+			const void *data = buf->peek();
+			int32_t be32 = *static_cast<const int32_t*>(data);
+			buf->retrieve(sizeof(int32_t));
+			time_t time = sockets::networkToHost32(be32);
+			Timestamp ts(implicit_cast<uint64_t>(time) *
+					Timestamp::kMicroSecondsPerSecond);
+			LOG_INFO << "Server time = " << time << ", " 
+				<< ts.toFormattedString();
+		} 
+		else
+		{
+			LOG_INFO << conn->name() << " no enough data " 
+				<< buf->readableBytes() << " at "
+				<< receiveTime.toFormattedString();
+		}	
+	}
+};
+
+int main(int argc,char *argv[])
+{
+	LOG_INFO << "pid = " << getpid();
+	if(argc > 1)
+	{
+		EventLoop loop;
+		InetAddr serverAddr(argv[1],2037);
+		TimeClient timeClient(&loop,serverAddr);
+		timeClient.connect();
+		loop.loop();
+	}
+	else
+	{
+		printf("Usage: %s host_ip\n",argv[0]);
+	}
+
+}
