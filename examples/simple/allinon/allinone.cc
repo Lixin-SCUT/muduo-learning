7d310877499f9c5160c25d02f08a86e7d8871ac2
Lixin-SCUT <im.lixin@foxmail.com>
2020-02-23 12:28:34 +0800

    2020.02.23

---
+++ examples/simple/allinon/allinone.cc
@@ -0,0 +1,39 @@
+// allinone.cc
+// Created by Lixin on 2020.02.23
+
+#include "examples/simple/chargen/chargen.h"
+#include "examples/simple/daytime/daytime.h"
+#include "examples/simple/discard/discard.h"
+#include "examples/simple/echo/echo.h"
+#include "examples/simple/time/time.h"
+
+#include "muduo/base/Logging.h"
+#include "muduo/net/EventLoop.h"
+
+#include <unistd.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+int main()
+{
+	LOG_INFO << "pid = " << getpid();
+	EventLoop *loop; // All servers will share one loop
+
+	ChargenServer chargenServer(&loop, InetAddress(2019));
+	chargenServer.start();
+
+	DaytimeServer daytimeServer(&loop, InetAddress(2013));
+	daytimeServer.start();
+
+	DiscardServer discardServer(&loop, InetAddress(2009));
+	discardServer.start();
+
+	EchoServer echoServer(&loop, InetAddress(2007));
+	echoServer.start();
+
+	TimeServer timeServer(&loop, InetAddress(2037));
+	timeServer.start();
+
+	loop.loop();
+}
