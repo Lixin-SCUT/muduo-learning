88c1fe1e1188175cc5334cb566e528787fda97c2
Lixin-SCUT <im.lixin@foxmail.com>
2020-02-27 23:55:08 +0800

    2020.02.27

--- examples/asio/chat/client.cc
+++ examples/asio/chat/client.cc
@@ -25,12 +25,82 @@ public:
 		client_.setConnectionCallback(
 				std::bind(&onConnection,this,_1));
 		client_.setMessageCallback(
-				std::bind(&LengthHeaderCodec::onMessgage,&code_,_1,_2,_3));
+				std::bind(&LengthHeaderCodec::onMessgage,&codec_,_1,_2,_3));
 		client_.enableRetry();
 	}
+
+	void connect()
+	{
+		client_.connect();
+	}
+
+	void disconnect()
+	{
+		client_.disconnect();
+	}
+
+	void write(const StringPiece& message)
+	{
+		MutexLockGuard lock(mutex_);
+		if(connection_)
+		{
+			codec_.send(connection_.get(),message);
+		}
+	}
+
+private:
+	void onConnection(const TcpConnectionPtr& conn)
+	{
+		LOG_INFO << conn->localAddress().toIpPort() << " -> "
+             		<< conn->peerAddress().toIpPort() << " is "
+             		<< (conn->connected() ? "UP" : "DOWN");
+		
+		MutexLockGuard lock(mutex_);
+		if(conn->connected())
+		{
+			connection_ = conn;
+		}
+		else
+		{
+			connection_.reset();
+		}
+	}
+
+	void onStringMessage(const TcpConnectionPtr&,
+				const string& message,
+				Timestamp)
+	{
+		pring("<<< %s\n",message.c_str());
+	}
+
 private:
 	TcpClient clent_;
 	LengthHeaderCodec codec_;
 	MutexLock mutex_;
 	TcpConnectionPtr connection_ GUARDED_BY(mutex_);
 };
+
+int main(int argc, char* argv[])
+{
+	LOG_INFO << "pid = " << getpid();
+	if(argc >2)
+	{
+		EventLoopThread loopThread;
+		unit16_t port = static_cast<unit16_t>(atoi(argv[2]));
+		InetAddress serverAddr(argv[1],port);
+
+		ChatClient client(&loopThread.startLoop(),serverAddr);
+		client.connect();
+		std::string line;
+		while(std::getline(std::cin,line))
+		{
+			client.write(line);
+		}
+		client.disconnect();
+		CurrentThread::sleepUsec(1000*1000); // wait for disconnect
+	}
+	else 
+	{
+		printf("Usage: %s host_ip,port\n",argv[0]);
+	}
+}
